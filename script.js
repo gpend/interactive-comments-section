8496b3890b3194560b6592985307da57837b2092
greg <gpendleton@gmail.com>
2022-06-12 21:29:29 -0500

    add comments from file

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ fetch("./data.json")
     document.querySelector(".user-section").innerHTML = getUserBlockHTML(
       data.currentUser
     );
+    // @ts-ignore
+    document.querySelector(".comment-section").innerHTML = displayComments(data.comments)
   });
 
 function getUserBlockHTML(userInfo) {
@@ -12,4 +14,42 @@ function getUserBlockHTML(userInfo) {
                     <textarea name="add-comment">add a comment</textarea>
                     <button class='comment-submit'>send</button>`;
   return html;
+}
+
+function getCommentHTML(commentData){
+  console.log(commentData)
+  const html = `<div class="comment-card">
+                  <div class="comment-card--header">
+                      <img src="${commentData.user.image.webp}" alt="user avatar">
+                      <div class="comment-card--header__userID"> ${commentData.user.username} </div>
+                      <div class="comment-card--header__createdAt" ${commentData.createdAt}</div>
+                  </div>
+                  <div class="comment-card--body">${commentData.content}</div>
+                  <div class="comment-card--footer"></div>
+                </div>`
+  
+  return html
+}
+
+function displayComments(data){
+  let comments =""
+  for (let comment of data){
+    let commentHTML = getCommentHTML(comment)
+    // @ts-ignore
+    for (let reply of comment.replies){
+      commentHTML += `<div class="comment--reply">${getCommentHTML(reply)}</div>`
+    }
+    console.log(commentHTML)
+    comments += commentHTML
+  }
+  return comments
+}
+
+
+class comment{
+
+}
+
+class reply extends comment{
+  
 }
