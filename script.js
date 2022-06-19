72afb7cc41c709b610b20f9c6efd51ed46cf6964
greg <gpendleton@gmail.com>
2022-06-19 16:09:00 -0500

    tweak styling and formatting

--- script.js
+++ script.js
@@ -1,53 +1,64 @@
-fetch("./data.json")
+dfetch('./data.json')
   .then((response) => response.json())
   .then((data) => {
-    // @ts-ignore
-    document.querySelector(".user-section").innerHTML = getUserBlockHTML(
+    document.querySelector('.user-section').innerHTML = getUserBlockHTML(
       data.currentUser
     );
-    // @ts-ignore
-    document.querySelector(".comment-section").innerHTML = displayComments(data.comments)
+
+    document.querySelector('.comment-section').innerHTML = displayComments(
+      data.comments
+    );
   });
 
 function getUserBlockHTML(userInfo) {
   const html = `<img src='${userInfo.image.webp}' alt='user pic'/>
                     <textarea name="add-comment">add a comment</textarea>
                     <button class='comment-submit'>send</button>`;
+
   return html;
 }
 
-function getCommentHTML(commentData){
-  console.log(commentData)
+function getCommentHTML(commentData) {
   const html = `<div class="comment-card--header">
                       <img src="${commentData.user.image.webp}" alt="user avatar">
                       <div class="comment-card--header__userID"> ${commentData.user.username} </div>
                       <div class="comment-card--header__createdAt"> ${commentData.createdAt}</div>
                   </div>
                   <div class="comment-card--body">${commentData.content}</div>
-                  <div class="comment-card--footer"></div>`
-  
-  return html
+                  <div class="comment-card--footer">
+                      <div class="comment-card--footer__score">${commentData.score}</div>
+                      <div class="comment-card--footer__buttons"></div>
+                      <div class="comment-card--replies>${}</div>
+                  </div>`;
+
+
+    // add delete and edit button to current users comments
+    //add reply button to other users comments
+  return html;
 }
 
-function displayComments(data){ //TODO fix nesting
-  let comments =""
-  for (let comment of data){
-    let commentHTML = `<div class="comment-card">${getCommentHTML(comment)}</div>`
-    // @ts-ignore
-    for (let reply of comment.replies){
-      commentHTML += `<div class="comment-card comment-reply">${getCommentHTML(reply)}</div>`
+function displayComments(data) {
+  let comments = '';
+
+    for (let comment of data) {
+      let commentHTML = `<div class="comment-card">${getCommentHTML(
+        comment
+      )}</div>`;
     }
-    console.log(commentHTML)
-    comments += commentHTML
+    
+    for (let reply of comment.replies) { 
+      //make this a separate function to use inside the getCommentHTML
+      commentHTML += `<div class="comment-card comment-reply">${getCommentHTML(
+        reply
+      )}</div>`;
+    } 
+
+    comments += commentHTML;
   }
-  return comments
+
+  return comments;
 }
 
+// class comment {}
 
-class comment{
-
-}
-
-class reply extends comment{
-
-}
+// class reply extends comment {}
