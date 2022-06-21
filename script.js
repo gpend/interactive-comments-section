22047be24053ec3e194fb290e028b6081af7aec4
greg <gpendleton@gmail.com>
2022-06-21 17:02:02 -0500

    add footer buttons

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ fetch('./data.json')
     );
 
     document.querySelector('.comment-section').innerHTML = displayComments(
-      data.comments
+      data.comments,
+      data.currentUser
     );
   });
 
@@ -18,37 +19,53 @@ function getUserBlockHTML(userInfo) {
   return html;
 }
 
-function getCommentHTML(commentData) {
+function getCommentHTML(commentData, currentUser) {
   const html = `<div class="comment-card--header">
-                      <img src="${commentData.user.image.webp}" alt="user avatar">
-                      <div class="comment-card--header__userID"> ${commentData.user.username} </div>
-                      <div class="comment-card--header__createdAt"> ${commentData.createdAt}</div>
+                      <img src="${
+                        commentData.user.image.webp
+                      }" alt="user avatar">
+                      <div class="comment-card--header__userID"> ${
+                        commentData.user.username
+                      } </div>
+                      <div class="comment-card--header__createdAt"> ${
+                        commentData.createdAt
+                      }</div>
                 </div>
                 <div class="comment-card--body">${commentData.content}</div>
                 <div class="comment-card--footer">
-                    <div class="comment-card--footer__score">${commentData.score}</div>
-                    <div class="comment-card--footer__buttons"></div>
-                </div>
-                  `;
+                    <div class="comment-card--footer__score-controls">
+                        <button class="comment-card--footer__score-plus"> + </button>
+                        <div class="comment-card--footer__score">${
+                          commentData.score
+                        }</div>
+                        <button class="comment-card--footer__score-minus"> - </button>
+                    </div>
+                    <div class="comment-card--footer__comment-controls">
+                       ${getFooterButton(commentData, currentUser)}
+                    </div>
+                </div>`;
 
   // add delete and edit button to current users comments
   //add reply button to other users comments
   return html;
 }
 
-function displayComments(data) {
+function displayComments(data, currentUser) {
   let comments = '';
 
   for (let comment of data) {
     let commentHTML = `<div class="comment-card">${getCommentHTML(
-      comment
+      comment,
+      currentUser
     )}</div>`;
 
     commentHTML += '<div class="comment-card__replies">';
 
     for (let reply of comment.replies) {
+      // console.log(reply)
       let commentReplyHTML = `<div class="comment-card comment-reply">${getCommentHTML(
-        reply
+        reply,
+        currentUser
       )}</div>`;
       commentHTML += commentReplyHTML;
     }
@@ -59,6 +76,12 @@ function displayComments(data) {
   }
 
   return comments;
+}
+
+function getFooterButton(comment, currentUser) {
+  return comment.user.username != currentUser.username
+    ? '<button class="reply-button">reply</button>'
+    : '<button class="edit-button">edit</button> <button class="delete-button">delete</button>';
 }
 
 // class comment {}
@@ -72,3 +95,5 @@ function displayComments(data) {
 //     ? ''
 //     : getReplyHTML(commentData)}
 // </div>;
+
+// ${if (commentData.replyingTo){console.log(true)}}
